e675ef9c70ac4d5a925d105536b480cfd797d1eb
xiaoxinmiao <xiaoxm_001@163.com>
2022-10-15 14:33:01 +0800

    first commit

---
+++ b8/b8.go
@@ -0,0 +1,7 @@
+package b8
+
+import "github.com/relax-space/go-a8/a8"
+
+func Hello() string {
+	return a8.Hello()
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/relax-space/go-b8
+
+go 1.19
+
+require github.com/relax-space/go-a8 v1.1.0
